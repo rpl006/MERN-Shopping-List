1d554dcf68abcd7682fa6f442f2ee34e8a11054b
DWproduction5 <dwproduction5@DWproduction5.local>
2019-05-01 15:08:35 -0500

    Redux Delete State

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,43 +1,23 @@
 import React, { Component } from "react";
 import { Container, ListGroup, ListGroupItem, Button } from "reactstrap";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
-import uuid from "uuid";
 import { connect } from "react-redux";
-import { getItems } from "../actions/itemActions";
+import { getItems, deleteItem } from "../actions/itemActions";
 import PropTypes from "prop-types";
 
 class ShoppingList extends Component {
-  /*state = {
-    items: [
-      { id: uuid(), name: "Eggs" },
-      { id: uuid(), name: "Milk" },
-      { id: uuid(), name: "Steak" },
-      { id: uuid(), name: "Meat" }
-    ]
-  };
-  */
   componentDidMount() {
     this.props.getItems();
   }
+
+  onDeleteClick = id => {
+    this.props.deleteItem(id);
+  };
 
   render() {
     const { items } = this.props.item;
     return (
       <Container>
-        <Button
-          color="info"
-          style={{ marginBottom: "2rem" }}
-          onClick={() => {
-            const name = prompt("Enter Item");
-            if (name) {
-              this.setState(state => ({
-                items: [...state.items, { id: uuid(), name: name }]
-              }));
-            }
-          }}
-        >
-          Add Item
-        </Button>
         <ListGroup>
           <TransitionGroup className="shopping-list">
             {items.map(({ id, name }) => (
@@ -47,11 +27,7 @@ class ShoppingList extends Component {
                     className="remove-btn"
                     color="danger"
                     size="sm"
-                    onClick={() => {
-                      this.setState(state => ({
-                        items: state.items.filter(item => item.id !== id)
-                      }));
-                    }}
+                    onClick={this.onDeleteClick.bind(this, id)}
                     style={{ marginRight: "0.5rem" }}
                   >
                     &times;
@@ -78,5 +54,5 @@ const mapStateToProps = state => ({
 
 export default connect(
   mapStateToProps,
-  { getItems }
+  { getItems, deleteItem }
 )(ShoppingList);

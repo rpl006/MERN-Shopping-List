0d1e65598c962c81e91d634114c907bf11d3f61f
DWproduction5 <dwproduction5@DWproduction5.local>
2019-05-01 16:09:48 -0500

    ItemModal ,and its settings added

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import AppNavbar from "./components/AppNavbar";
 import Footer from "./components/AppFooter";
 import ShoppingList from "./components/ShoppingList";
+import { Container } from "reactstrap";
 
 import { Provider } from "react-redux";
 import store from "./store";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import ItemModal from "./components/ItemModal";
 
 class App extends Component {
   render() {
@@ -15,7 +17,11 @@ class App extends Component {
       <Provider store={store}>
         <div className="App">
           <AppNavbar />
-          <ShoppingList />
+          <Container>
+            <ItemModal />
+            <ShoppingList />
+          </Container>
+
           <Footer />
         </div>
       </Provider>

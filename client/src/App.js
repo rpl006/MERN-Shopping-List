fb35ee5c95fc4df7a7abae6ee53097616663f82e
DWproduction5 <dwproduction5@DWproduction5.local>
2019-05-01 13:57:35 -0500

    Redux settings added

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,22 @@ import AppNavbar from "./components/AppNavbar";
 import Footer from "./components/AppFooter";
 import ShoppingList from "./components/ShoppingList";
 
+import { Provider } from "react-redux";
+import store from "./store";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <AppNavbar />
-        <ShoppingList />
-        <Footer />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <AppNavbar />
+          <ShoppingList />
+          <Footer />
+        </div>
+      </Provider>
     );
   }
 }

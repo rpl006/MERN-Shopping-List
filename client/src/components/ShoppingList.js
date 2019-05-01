45ae628b5be3bd310b89d77790c34005ef253196
DWproduction5 <dwproduction5@DWproduction5.local>
2019-05-01 13:23:34 -0500

    Shopping List transition , Transition Group added

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Container, ListGroup, ListGroupItem, Button } from "reactstrap";
-import { CSSTransition, Transition } from "react-transition-group";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import uuid from "uuid";
 
 class ShoppingList extends Component {
@@ -22,10 +22,39 @@ class ShoppingList extends Component {
           style={{ marginBottom: "2rem" }}
           onClick={() => {
             const name = prompt("Enter Item");
+            if (name) {
+              this.setState(state => ({
+                items: [...state.items, { id: uuid(), name: name }]
+              }));
+            }
           }}
         >
           Add Item
         </Button>
+        <ListGroup>
+          <TransitionGroup className="shopping-list">
+            {items.map(({ id, name }) => (
+              <CSSTransition key={id} timeout={500} classNames="fade">
+                <ListGroupItem>
+                  <Button
+                    className="remove-btn"
+                    color="danger"
+                    size="sm"
+                    onClick={() => {
+                      this.setState(state => ({
+                        items: state.items.filter(item => item.id !== id)
+                      }));
+                    }}
+                    style={{ marginRight: "0.5rem" }}
+                  >
+                    &times;
+                  </Button>
+                  {name}
+                </ListGroupItem>
+              </CSSTransition>
+            ))}
+          </TransitionGroup>
+        </ListGroup>
       </Container>
     );
   }

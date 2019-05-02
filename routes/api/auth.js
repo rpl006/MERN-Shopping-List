03735cfc62e48f186c50dff4f22f9fdd952f2320
DWproduction5 <dwproduction5@DWproduction5.local>
2019-05-02 13:01:45 -0500

    Middleware added, and settings to it

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 const config = require("config");
 const jwt = require("jsonwebtoken");
+const auth = require("../../middleware/auth");
 
 //User Model
 const User = require("../../models/User");
@@ -49,6 +50,16 @@ router.post("/", (req, res) => {
       );
     });
   });
+});
+
+//@route GET api/auth/user
+//@desc Get user data
+//@access Private
+
+router.get("/user", auth, (req, res) => {
+  User.findById(req.user.id)
+    .select("-password")
+    .then(user => res.json(user));
 });
 
 module.exports = router;

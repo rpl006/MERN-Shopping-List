2399877c23612e4a8fca4cdcbb4b128ce0818b13
DWproduction5 <dwproduction5@DWproduction5.local>
2019-05-02 10:27:02 -0500

    User Schema added

---
+++ models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//Create Schema
+
+const Userchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  register_date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = User = mongoose.model("user", ItemSchema);

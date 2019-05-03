ce00d92752fe883ca0450148bdc55f0cb1c8ba07
DWproduction5 <dwproduction5@DWproduction5.local>
2019-05-03 09:23:41 -0500

    Error reucer ,and auth reducer settings

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from "redux";
 import itemReducer from "./itemReducer";
+import authReducer from "./autheReducer";
+import errorReducer from "./errorReducer";
 
 export default combineReducers({
-  item: itemReducer
+  item: itemReducer,
+  error: errorReducer,
+  auth: authReducer
 });

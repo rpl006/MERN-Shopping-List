6e1ef8fe4743fd296355dc1c03a15f5eb19e7530
DWproduction5 <dwproduction5@DWproduction5.local>
2019-05-06 13:27:15 -0500

    Add items/ delete button settings based in user credentials

--- client/src/components/ItemModal.js
+++ client/src/components/ItemModal.js
@@ -11,12 +11,17 @@ import {
 } from "reactstrap";
 
 import { connect } from "react-redux";
+import PropTypes from "prop-types";
 import { addItem } from "../actions/itemActions";
 
 class ItemModal extends Component {
   state = {
     modal: false,
     name: ""
+  };
+
+  static propTypes = {
+    isAuthenticated: PropTypes.bool
   };
 
   toggle = () => {
@@ -46,13 +51,18 @@ class ItemModal extends Component {
   render() {
     return (
       <div>
-        <Button
-          color="dark"
-          style={{ marginBottom: "2rem" }}
-          onClick={this.toggle}
-        >
-          Add Item
-        </Button>
+        {this.props.isAuthenticated ? (
+          <Button
+            color="dark"
+            style={{ marginBottom: "2rem" }}
+            onClick={this.toggle}
+          >
+            Add Item
+          </Button>
+        ) : (
+          <h4 className="mb-3 ml-3">Please login to manage items.</h4>
+        )}
+
         <Modal isOpen={this.state.modal} toggle={this.toggle}>
           <ModalHeader toggle={this.toggle}>Add To Shopping List</ModalHeader>
           <ModalBody>
@@ -79,7 +89,8 @@ class ItemModal extends Component {
 }
 
 const mapStateToProps = state => ({
-  item: state.item
+  item: state.item,
+  isAuthenticated: state.auth.isAuthenticated
 });
 
 export default connect(
--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -6,6 +6,12 @@ import { getItems, deleteItem } from "../actions/itemActions";
 import PropTypes from "prop-types";
 
 class ShoppingList extends Component {
+  static propTypes = {
+    getItems: PropTypes.func.isRequired,
+    item: PropTypes.object.isRequired,
+    isAuthenticated: PropTypes.bool
+  };
+
   componentDidMount() {
     this.props.getItems();
   }
@@ -23,15 +29,20 @@ class ShoppingList extends Component {
             {items.map(({ _id, name }) => (
               <CSSTransition key={_id} timeout={500} classNames="fade">
                 <ListGroupItem>
-                  <Button
-                    className="remove-btn"
-                    color="danger"
-                    size="sm"
-                    onClick={this.onDeleteClick.bind(this, _id)}
-                    style={{ marginRight: "0.5rem" }}
-                  >
-                    &times;
-                  </Button>
+                  {this.props.isAuthenticated ? (
+                    <Button
+                      className="remove-btn"
+                      color="danger"
+                      size="sm"
+                      onClick={this.onDeleteClick.bind(this, _id)}
+                      style={{ marginRight: "0.5rem" }}
+                    >
+                      &times;
+                    </Button>
+                  ) : (
+                    ""
+                  )}
+
                   {name}
                 </ListGroupItem>
               </CSSTransition>
@@ -43,13 +54,9 @@ class ShoppingList extends Component {
   }
 }
 
-ShoppingList.propTypes = {
-  getItems: PropTypes.func.isRequired,
-  item: PropTypes.object.isRequired
-};
-
 const mapStateToProps = state => ({
-  item: state.item
+  item: state.item,
+  isAuthenticated: state.auth.isAuthenticated
 });
 
 export default connect(

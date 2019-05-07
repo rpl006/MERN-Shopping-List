c5d9f1a3153f10caa70abe4c9d8670ee617bd50c
DWproduction5 <dwproduction5@DWproduction5.local>
2019-05-07 12:31:54 -0500

    Test

--- server.js
+++ server.js
@@ -38,4 +38,6 @@ if (process.env.NODE_ENV === "production") {
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server Started on Port ${port}`));
+app.listen(port, () => {
+  console.log(`Server Started on Port ${port}`);
+});

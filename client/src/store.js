8ce1442ef04e5c23b6c65131d6200d9997add25e
Ram Lamichhane <ram.lamichhane@aol.com>
2019-06-12 15:14:43 -0500

    Fixed the compose function for devtools extension

--- client/src/store.js
+++ client/src/store.js
@@ -4,15 +4,19 @@ import rootReducer from "./reducers";
 
 const initialState = {};
 
-const middleware = [thunk];
+const middleware = [
+  applyMiddleware(thunk),
+  ...(window.__REDUX_DEVTOOLS_EXTENSION__
+    ? [window.__REDUX_DEVTOOLS_EXTENSION__()]
+    : [])
+];
 
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  compose(...middleware)
+
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 export default store;
